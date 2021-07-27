7b6a865b043309e17545dcf0d3625154fc22e65c
LeeZhiYu <203783Q@mymail.nyp.edu.sg>
2021-07-27 17:03:17 +0800

    init: register app route got changed so that it will auto login after registering

--- __init__.py
+++ __init__.py
@@ -25,7 +25,7 @@ app.secret_key = 'any_random_string'
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Fccfxx322399'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'sia'
 
 mysql = MySQL(app)
